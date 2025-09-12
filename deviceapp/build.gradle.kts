b2d7a24919aeaa19b502414d1a23a6857d9b1108
alaintanguy <61603038+alaintanguy@users.noreply.github.com>
2025-09-11 21:30:42 -0700

    Update build.gradle.kts

--- deviceapp/build.gradle.kts
+++ deviceapp/build.gradle.kts
@@ -11,6 +11,13 @@ android {
     targetSdk = 34
     versionCode = 1
     versionName = "1.0"
+compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+  kotlinOptions {
+    jvmTarget = "17"
+  }
   }
   buildTypes {
     release {
